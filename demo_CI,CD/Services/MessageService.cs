f8bdf1a4db4476a5683dbc2e3205f6207f81d942
Nguyen Xuan Cuong <tohuuc14@gmail.com>
2026-09-24 15:27:05 +0700

    Da them file implement moi MessageService.cs

---
+++ demo_CI,CD/Services/MessageService.cs
@@ -0,0 +1,10 @@
+﻿namespace demo_CI_CD.Services
+{
+    public class MessageService : IMessageService
+    {
+        public string GetWelcomeMessage()
+        {
+            return "Chao mung ban den voi ung dung ASP.NET Core MVC (CI/CD Demo)!";
+        }
+    }
+}
